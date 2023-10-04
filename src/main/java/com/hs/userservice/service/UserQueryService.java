ef1e448ca70e437be3e469eb18f49b18a561942b
shahih25 <hmshahid@asu.edu>
2023-10-03 18:48:49 -0700

    Testing pagination

--- src/main/java/com/hs/userservice/service/UserQueryService.java
+++ src/main/java/com/hs/userservice/service/UserQueryService.java
@@ -4,9 +4,13 @@ import com.hs.userservice.model.User;
 import com.hs.userservice.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -16,6 +20,15 @@ public class UserQueryService {
     UserRepository userRepository;
 
     public User getUserByUsername(final String username) {
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("firstName").ascending());
+        Page<User> users = userRepository.findAll(pageable);
+        List<User> usersAll = userRepository.findAll();
+        return userRepository.findById(username).orElseThrow();
+    }
+
+    public User getAllUsers(final String username) {
+        Pageable pageable = PageRequest.of(1, 10, Sort.Direction.ASC);
+        Page<User> users = userRepository.findAll(pageable);
         return userRepository.findById(username).orElseThrow();
     }
 }
